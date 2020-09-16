732658519a49ee29de2d71f8be5af24222a18aa4
Crowdbotics <team@crowdbotics.com>
2020-09-16 07:04:44 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,11 +1,16 @@
 import axios from "axios"
+const plugin13 = axios.create({
+  baseURL:
+    "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/7238/storyboard/8485/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 const plugin11 = axios.create({
   baseURL:
     "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/7307/storyboard/8491/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 const mobile16septAPI = axios.create({
-  baseURL: "https://mobile-16-sept-dev-11032.botics.co/",
+  baseURL: "https://mobile-16-sept-dev-11032-prod.herokuapp.com/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function api_v1_customtext_list() {
